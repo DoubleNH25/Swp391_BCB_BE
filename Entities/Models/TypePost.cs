9ce3a1fa25fe12da61cdf84d6456f1d89b0ff664
kakavinh <115615391+kakavinh@users.noreply.github.com>
2024-06-24 12:21:26 +0700

    update setting

--- Entities/Models/TypePost.cs
+++ Entities/Models/TypePost.cs
@@ -1,1 +1,15 @@
-﻿
+﻿namespace Entities.Models
+{
+    public partial class TypePost
+    {
+        public TypePost()
+        {
+            Posts = new HashSet<Post>();
+        }
+
+        public int Id { get; set; }
+        public string? TypePost1 { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
