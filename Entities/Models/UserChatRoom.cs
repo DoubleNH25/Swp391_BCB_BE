ad814e8f5c8219b0d447acc806092e819fdb3969
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2024-06-24 08:25:44 +0700

    update entities

--- Entities/Models/UserChatRoom.cs
+++ Entities/Models/UserChatRoom.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class UserChatRoom
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public int? RoomId { get; set; }
+
+        public User? User { get; set; }
+        public ChatRoom? ChatRoom { get; set; }
+    }
+}
