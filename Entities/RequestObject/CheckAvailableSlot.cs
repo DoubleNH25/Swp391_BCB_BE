f902ea3c69e19f2fe9c1df6c28c8142861e043d1
kakavinh <115615391+kakavinh@users.noreply.github.com>
2024-06-25 14:54:50 +0700

    setting

--- Entities/RequestObject/CheckAvailableSlot.cs
+++ Entities/RequestObject/CheckAvailableSlot.cs
@@ -1,1 +1,15 @@
-﻿
+﻿namespace Entities.RequestObject
+{
+    public class CheckAvailableSlot
+    {
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+        public List<SlotsInfo> SlotsInfo { get; set; }
+    }
+
+    public class SlotsInfo
+    {
+        public DateTime DateRegis { get; set; }
+        public int NumSlots { get; set; }
+    }
+}
