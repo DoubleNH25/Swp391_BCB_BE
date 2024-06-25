1dcab7af3953a92d22fc25683b3713a717801167
kakavinh <115615391+kakavinh@users.noreply.github.com>
2024-06-25 19:10:29 +0700

    m

--- Entities/RequestObject/TransactionCreateInfo.cs
+++ Entities/RequestObject/TransactionCreateInfo.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.RequestObject
+{
+    public class TransactionCreateInfo
+    {
+        public int? IdUser { get; set; }
+        public List<int>? IdSlot { get; set; }
+    }
+}
