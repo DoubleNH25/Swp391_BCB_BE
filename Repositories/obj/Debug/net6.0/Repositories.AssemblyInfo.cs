cf5a5e4ff791c6fcf4f92bb1ea87cde884928e53
Đặng Trung Kiên <kiendtse172414@fpt.edu.vn>
2024-06-26 00:30:40 +0700

    Update ResponseObject

--- Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7cfa82ac069bacfbc1e70ba8ae630f22af3b8b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+795cb6e82e9a026263df208813fa6368ccda60b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
