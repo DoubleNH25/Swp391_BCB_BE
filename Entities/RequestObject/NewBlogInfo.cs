32863db0a91428d3e1bfb0f8cba190139d61f88d
kakavinh <115615391+kakavinh@users.noreply.github.com>
2024-06-25 15:23:26 +0700

    setting

--- Entities/RequestObject/NewBlogInfo.cs
+++ Entities/RequestObject/NewBlogInfo.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace Entities.RequestObject
+{
+    public class NewBlogInfo
+    {
+        public string? Title { get; set; }
+        public string? Summary { get; set; }
+        public string? Description { get; set; }
+        public List<string>? ImgUrls { get; set; }
+        public string? HighlightImg { get; set; }
+
+    }
+}
