01872a1eb1989ae7390d97ced182736772549555
kakavinh <115615391+kakavinh@users.noreply.github.com>
2024-06-25 14:50:13 +0700

    change setting

--- BadmintonMatching/obj/Debug/net6.0/BadmintonMatching.AssemblyInfo.cs
+++ BadmintonMatching/obj/Debug/net6.0/BadmintonMatching.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadmintonMatching")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1258df642c45091950c598573d8f34b03138fb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadmintonMatching")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadmintonMatching")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
