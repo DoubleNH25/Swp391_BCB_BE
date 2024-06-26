ade36ae7fc9e6a5ef9f279381869551e3232cb2b
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2024-06-26 07:46:37 +0700

    Update ChatRoom repositories

--- Repositories/Implements/ChatRoomRepository.cs
+++ Repositories/Implements/ChatRoomRepository.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Entities;
+using Entities.Models;
+using Repositories.Intefaces;
+
+namespace Repositories.Implements
+{
+    public class ChatRoomRepository : RepositoryBase<ChatRoom>, IChatRoomRepository
+    {
+        public ChatRoomRepository(DataContext context) : base(context)
+        {
+
+        }
+    }
+}
