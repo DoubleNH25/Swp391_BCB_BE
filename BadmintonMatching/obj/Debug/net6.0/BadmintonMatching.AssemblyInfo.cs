7ddb70945479f0e07e1d024524dc52f0c594cbb6
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2024-06-26 07:51:06 +0700

    fix bug

--- BadmintonMatching/obj/Debug/net6.0/BadmintonMatching.AssemblyInfo.cs
+++ BadmintonMatching/obj/Debug/net6.0/BadmintonMatching.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadmintonMatching")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+070cbfa1c9b563c85acb2bbd461a6859ea6e63a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ade36ae7fc9e6a5ef9f279381869551e3232cb2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadmintonMatching")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadmintonMatching")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/obj/Debug/net6.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net6.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+070cbfa1c9b563c85acb2bbd461a6859ea6e63a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ade36ae7fc9e6a5ef9f279381869551e3232cb2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
