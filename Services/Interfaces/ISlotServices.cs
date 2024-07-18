e18e732d9fdaebb383a842878f3311cfc12de4b5
kakavinh <115615391+kakavinh@users.noreply.github.com>
2024-07-18 16:36:14 +0700

    F1

--- Services/Interfaces/ISlotServices.cs
+++ Services/Interfaces/ISlotServices.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using Entities.RequestObject;
+using Entities.ResponseObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface ISlotServices
+    {
+        void Delete(List<int> lsSlot);
+        List<SlotReturnInfo> GetAvailable(CheckAvailableSlot info);
+    }
+}
