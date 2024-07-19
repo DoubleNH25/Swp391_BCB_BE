4fe419da68b3b25887c654013668c742df9c2705
DoubleNH25 <158280553+KennyyDang@users.noreply.github.com>
2024-07-19 12:54:27 +0700

    update jwt support and chat

--- Services/Interfaces/IJwtSupport.cs
+++ Services/Interfaces/IJwtSupport.cs
@@ -1,1 +1,8 @@
-﻿
+﻿namespace Services.Interfaces
+{
+    public interface IJwtSupport
+    {
+        string CreateToken(int role, int accountId, bool isNewUser);
+        string CreateToken(string otp);
+    }
+}
