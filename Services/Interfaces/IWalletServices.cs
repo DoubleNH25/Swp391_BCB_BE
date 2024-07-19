ec1e5ac852dd207b1255251f0ed7bf76d81fec8a
kakavinh <115615391+kakavinh@users.noreply.github.com>
2024-07-19 15:35:43 +0700

    F1

--- Services/Interfaces/IWalletServices.cs
+++ Services/Interfaces/IWalletServices.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using Entities.Models;
+using Entities.ResponseObject;
+
+namespace Services.Interfaces
+{
+    public interface SlotServices
+    {
+        List<HistoryWalletModel> GetHistory(int user_id);
+        decimal UpdateBalance(decimal? changes, int user_id, bool isFromTran);
+        Task<Wallet> getWallerByUserId(int userId);
+    }
+}
