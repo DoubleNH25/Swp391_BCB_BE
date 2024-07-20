714d9193aed79abbdbcde5e6dc4c8be3b61921ea
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2024-07-20 08:20:21 +0700

    update AuthorizationFilter

--- BadmintonMatching/Extension/HangfireAuthorizationFilter.cs
+++ BadmintonMatching/Extension/HangfireAuthorizationFilter.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Hangfire.Dashboard;
+
+namespace BadmintonMatching.Extension
+{
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        private readonly string[] _roles;
+
+        public HangfireAuthorizationFilter(params string[] roles)
+        {
+            _roles = roles;
+        }
+
+        public bool Authorize(DashboardContext context)
+        {
+            var httpContext = ((AspNetCoreDashboardContext)context).HttpContext;
+
+            return true;
+        }
+    }
+}
