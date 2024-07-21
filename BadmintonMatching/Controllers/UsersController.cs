4a12c7de521d1d36aa16de822b5c2c5109ebb289
Đặng Trung Kiên <kiendtse172414@fpt.edu.vn>
2024-07-21 22:27:31 +0700

    Update code nho

--- BadmintonMatching/Controllers/UsersController.cs
+++ BadmintonMatching/Controllers/UsersController.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System.Net.WebSockets;
+using System.Web;
+using Entities.Models;
+using Entities.RequestObject;
+using Entities.ResponseObject;
+using Microsoft.AspNetCore.Mvc;
+using Services.Implements;
+using Services.Interfaces;
+
+namespace BadmintonMatching.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserServices _userServices;
+        private readonly IJwtSupport _jwtServices;
+        private readonly INotificationServices _notificationServices;
+
+        public UsersController(IUserServices userServices, IJwtSupport jwtServices, INotificationServices notificationServices)
+        {
+            _userServices = userServices;
+            _jwtServices = jwtServices;
+            _notificationServices = notificationServices;
+        }
+
+        [HttpPost]
+        [Route("email_login")]
+
+    }
+}
