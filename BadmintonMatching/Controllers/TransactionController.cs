080d70d59b41da0fcfece431cc73571fac72a0f1
Đặng Trung Kiên <kiendtse172414@fpt.edu.vn>
2024-07-21 22:21:14 +0700

    Update code

--- BadmintonMatching/Controllers/TransactionController.cs
+++ BadmintonMatching/Controllers/TransactionController.cs
@@ -24,7 +24,24 @@ namespace BadmintonMatching.Controllers
 
         [HttpPost]
         [Route("buy_slot")]
-      
+        public async Task<IActionResult> CreateTransactionBuyingSlot(TransactionCreateInfo info)
+        {
+
+            var tranId = await _transactionRepository.CreateForBuySlot(info);
+
+            if (tranId == 0)
+            {
+                return Ok(new SuccessObject<object> { Message = "Tạo giao dịch thất bại !" });
+            }
+            else if (tranId == -1)
+            {
+                return Ok(new SuccessObject<object> { Message = "Số tiền trong ví không đủ để thanh toán !" });
+            }
+            else
+            {
+                return Ok(new SuccessObject<object> { Data = new { TranSactionId = tranId }, Message = Message.SuccessMsg });
+            }
+        }
 
     }
 }
