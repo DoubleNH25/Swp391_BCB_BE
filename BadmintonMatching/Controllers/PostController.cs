fe2d5013b7e75057459078a7cdc91d0d51ca6691
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2024-07-22 15:10:55 +0700

    fix postcontroller

--- BadmintonMatching/Controllers/PostController.cs
+++ BadmintonMatching/Controllers/PostController.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using Entities.Models;
+using Entities.RequestObject;
+using Entities.ResponseObject;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Intefaces;
+using Services.Implements;
+using Services.Interfaces;
+using System.Text.Json.Nodes;
+
+namespace BadmintonMatching.Controllers
+{
+
+    [ApiController]
+    [Route("api/posts")]
+    public class PostController : ControllerBase
+    {
+        private List<string> PlaceList = new List<string>
+        {
+
+        };
+        private readonly IPostServices _postServices;
+        private readonly IUserServices _userServices;
+        private readonly INotificationServices _notificationServices;
+        private readonly IRepositoryManager _repositoryManager;
+
+
+        public PostController(IPostServices postServices, IUserServices userServices,
+            INotificationServices notificationServices, IRepositoryManager repositoryManager
+            )
+        {
+            _postServices = postServices;
+            _userServices = userServices;
+            _notificationServices = notificationServices;
+            _repositoryManager = repositoryManager;
+
+        }
+
+    }
+}
